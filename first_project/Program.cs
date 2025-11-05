177ece64838d6f8653584a58136d82baf0d0dda4
Sasha1234563 <shelkovin.54@gmail.com>
2025-11-05 09:49:30 +0300

    Добавьте файлы проекта.

---
+++ first_project/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int l = int.Parse(Console.ReadLine());
+        int r = int.Parse(Console.ReadLine());
+        Console.WriteLine(GenereateNum(l, r));
+    }
+static int GenereateNum(int l, int r)
+    {
+        int n = new Random().Next(l, r);
+        //Console.WriteLine(n);
+        return n;
+    }
+}
