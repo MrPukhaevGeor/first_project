f7e49b13eff7bd39e783fab93b08e924e7fe9126
Cap_Harlok <138770914+MrPukhaevGeor@users.noreply.github.com>
2025-11-13 11:40:50 +0300

    Update Program.cs

--- first_project/Program.cs
+++ first_project/Program.cs
@@ -1,175 +1,143 @@
-﻿using System.Linq.Expressions;
+using System;
+using System.Numerics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace sueta
+public class MainClass
 {
-    internal class Program
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-            //string word = Console.ReadLine();
-            //int num = Convert.ToInt32(Console.ReadLine());
-            //int[] numbers = [1, 2, 3, 4, 7];
-            //double s = Convert.ToDouble(Console.ReadLine());
-            char s = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine(TranslationCapitalLetter(s));
-        }
-        static bool CheckParity(int num)//1
-        {
-            bool check = false;
-            if (num % 2 == 0)
+        string ss = ("рандон кардон картон картоф");
+        string[] sss = ss.Split(' ');
+        string w = sss[new Random().Next(1, 4)]; ;
+        string ans = "******";
+        int f = 0;
+        int totalScore = 0;
+        int count = 0;
+        int money = 0;
+        while (f == 0)
+        {   string secctor = FindSector();
+            string hid = "";
+            Console.WriteLine("Вращайте барабан");
+            if (FindBank(secctor))
             {
-                check = true;
+                break;
+            }
+            else if (FindX2(secctor, totalScore))
+            {
+                continue;
+            }
+            else if (secctor == "+")
+            {
+                Console.WriteLine("Выберите номер любой буквы");
 
             }
-            return check;
-
-        }
-        static int Factorial(int num)//2
-        {
-            int factorial = 1;
-            for (int i = 1; i<=num; i++)
+            else if (secctor == "П")
             {
-                factorial = i * factorial;
-            }
-            return factorial;
-        }
-        static int CountVowels(string word)//3
-        {
-            string vowels = "aeiouy";
-            int countVowels = 0;
-            foreach (char c in word)
-            {
-				if (vowels.Contains(c))
-				{
-					countVowels++;
-				}
-			}
-            return countVowels;
-        }
-        static bool FindPolindrom(string word)//4
-        {
-            bool polindrom = false;
-            string newString = "";
-            for (int i = word.Length - 1;i >= 0; i--)
-            {
-                newString.Append(word[i]);
-            }
-            if (newString == word)
-            {
-                polindrom = true;
-            }
-            Console.Write(newString + " ");
-            return polindrom;
-        }
-        static int FindMax(int[] numbers)//5
-        {
-			int max = numbers[1];
-			for (int i = 0; i <= numbers.Length - 1; i++)
-            {
-                if (numbers[i] > max)
-                    max = numbers[i];
-            }
-            return max;
-        }
-        static bool FindPrimeNum(int num)//6
-        {
-            bool primeNum = false;
-            if (num < 2)
-                return primeNum;
-            for (int i = 2; i * i <= num; i++)
-            {
-                if (num % i == 0)
+                Console.WriteLine("Приз или деньги?");
+                string t = Console.ReadLine();
+                if (t == "Приз")
                 {
-                    return primeNum;
+                    Console.WriteLine("Поздравляю, вы выиграли банан, гуляйте");
+                    break;
                 }
-            }
-            primeNum = true;
-            return primeNum;
-        }
-        static char TranslationCapitalLetter(char s)//7
-        {
-            char[] Letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-			for (int i = 0; i < Letters.Length - 1; i++)
-            {
-                if (s == Letters[i])
+                else
                 {
-                    s = char.ToUpper(s);
+                    Console.WriteLine("Поздравляю, вы выиграли 100р, гуляйте");
+                    break;
                 }
+
             }
-            return s;
-        }
-        static string MakeCamelCase(string word)//8
-        {
-            string[] words = word.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            if (words.Length == 1)
-                return words[0].ToLower();
-            string result = words[0].ToLower();
-            for (int i = 1; i < words.Length; i++)
+             
+
+            else
             {
-                string wordy = words[i];
-                if (!string.IsNullOrEmpty(wordy))
+                int y = int.Parse(secctor);
+                Console.WriteLine("Выпало баллов " + y);
+                Console.WriteLine("угадывайте букву");
+                string inp = Console.ReadLine();
+                for (int i = 0; i < 5; i++)
                 {
-                    result += char.ToUpper(wordy[0]) + wordy.Substring(1).ToLower();
+
+                    if ((inp[0] == w[i]) && (ans.Contains(inp) == false))
+                    {
+                        hid += inp;
+                        totalScore += y;
+                        count += 1;
+
+
+                    }
+                    else
+                    {
+                        hid += ans[i];
+                        count = 0;
+
+
+                    }
+                    ans = hid;
+                    Console.WriteLine(hid);
+                    Console.WriteLine("Сумма баллов " + totalScore);
+
                 }
+
+
             }
-
-            return result;
-        }
-        static bool ValidPhoneNumber(string word)
-        {
-            if (string.IsNullOrEmpty(word))
-                return false;
-            string[] parts = word.Split('-');
-            if (parts.Length != 5)
-                return false;
-            if (parts[0] != "+7")
-                return false;
-
-            if (parts[1].Length != 3 || !IsAllDigits(parts[1])) return false;
-            if (parts[2].Length != 3 || !IsAllDigits(parts[2])) return false;
-            if (parts[3].Length != 2 || !IsAllDigits(parts[3])) return false;
-            if (parts[4].Length != 2 || !IsAllDigits(parts[4])) return false;
-
-            return true;
-        }
-        static bool IsAllDigits(string word)//9
-        {
-            foreach (char c in word)
+            if (count == 3)
             {
-                if (!char.IsDigit(c))
-                    return false;
-            }
-            return true;
-        }
-        static string FormatMoney(double s)//10
-        {
-            return s.ToString("N2").Replace(",", " ");
-        }
-        static int[] WithdrawMoneySimple(int num, int[] numbers)//11
-        {
-            Array.Sort(numbers);
-            Array.Reverse(numbers);
-            int[] counts = new int[numbers.Length];
-            int remaining = num;
 
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                counts[i] = remaining / numbers[i];
-                remaining %= numbers[i];
-            }
-            if (remaining != 0)
-                return new int[0];
+                FindPrise(money, count, )
 
-            List<int> result = new List<int>();
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                for (int j = 0; j < counts[i]; j++)
-                {
-                    result.Add(numbers[i]);
-                }
+
             }
-            return result.ToArray();
         }
+
+
     }
+    static bool FindBank(string s)
+    {   if (s == "Б")
+        {
+            Console.WriteLine("Банкрот");
+            return true;
+        }
+        return false;
+    }
+    static bool FindX2(string s, int x)
+    {
+        if (s == "x2")
+        {
+            Console.WriteLine("Очки умножаются на 2");
+            x *= 2;
+            return true;
+        }
+        return false;
+    }
+    static bool FindPlus(string s, int x)
+    {
+        if (s == "x2")
+        {
+            Console.WriteLine("Очки умножаются на 2");
+            x *= 2;
+            return true;
+        }
+        return false;
+    }
+    static string FindSector()
+    {
+        string sectors = ("Б П x2 + 350 450 550 500 750 850 700 600 650");
+        string[] lst_sectors = sectors.Split(' ');
+        return lst_sectors[new Random().Next(lst_sectors.Length)];
+    }
+    static bool FindPrise(int money, int count, int cc)
+    {
+        Console.WriteLine("В какой шкатулке 5 000. 1 , 2 или 3");
+        int ccn = new Random().Next(1, 3);
+        if (cc == ccn)
+        {
+            Console.WriteLine("ПОБЕДА");
+            money += 5000;
+            count = 0;
+            return true;
+        }
+        else { Console.WriteLine("эх"); count = 0; return false; }
+    }
+
 }
